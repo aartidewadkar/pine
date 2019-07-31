275a1fb17a89546f92c89797efdc4f9922720fbd
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2019-08-01 00:51:16 +0530

    added js

---
+++ public/js/pine.js
@@ -0,0 +1,9 @@
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+        $(".navbar").addClass("active");
+        // alert("this is javascript");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".navbar").removeClass("active");
+    }
+});
