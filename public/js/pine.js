493a14f3f819249790822f3b0894b7a38664aeca
Dnyaneshwar Dewadkar <dewadkar@gmail.com>
2019-08-09 10:38:56 +0530

    responsive contact page

--- public/js/pine.js
+++ public/js/pine.js
@@ -135,6 +135,17 @@ $(window).on("scroll", function () {
   // }
 
 
+  // $('.modal-content').resizable({
+  //   //alsoResize: ".modal-dialog",
+  //   minHeight: 300,
+  //   minWidth: 300
+  // });
+  // $('.modal-dialog').draggable();
 
+  // $('#exampleModal').on('show.bs.modal', function () {
+  //   $(this).find('.modal-body').css({
+  //     'max-height': '100%'
+  //   });
+  // });
 
 });
